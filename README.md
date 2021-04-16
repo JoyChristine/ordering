c43ddea482bcff6d5b5029cd64a4a375ba6c9ecb
Joy Christine <joychristin2@gmail.com>
2021-04-16 21:56:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,50 +1,14 @@
+ # Food ordering app using NODE JS, MONGODB and Express. 
+  To get started, you have to fork this repo to your own GitHub account first. Then open up a terminal on your machine and enter the following commands  
 
-# Food Ordering API
-
-To spin up both, the server in a watch mode for local development, execute:
-
-```bash
-git clone https://github.com/DickensJuma/OaklyOrders.git
-cd OaklyOrders
+  ```bash
+git clone https://github.com/JoyChristine/ordering.git
+cd ordering
 npm install
 npm start
 
-npm test
-
-# create a .env and replace the cred with real ones
-```
-
-
-Open [http://localhost:3000](http://localhost:3000) to test the server in the browser or Postman. 
-
-## SMS API
- [`Africanstalking SMS`](https://africastalking.com/sms)
-
-
-## Database
-[`MongoDB Atlas`](https://www.mongodb.com/cloud/atlas)
-
-# menu
-
-| Route |  HTTP Verb |
-| --- | --- |
-| /api/menu/ |	`GET` |
-| /api/menu/:id |	`GET` |
-| /api/menu/:id | `PATCH` |
-| /api/menu/ |	`POST` |
-| /api/menu/:id |	`DELETE` |
-
-
-# orders
-
-| Route |  HTTP Verb |
-| --- | --- |
-| /api/orders/ | `GET` |
-| /api/order/:id | `GET` |
-| /api/order/ | `POST` |
-
-
-
+Open [http://localhost:8080](http://localhost:8080) to test the server in the browser
+You can also use Postman like I did.
 
 
 
--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -25,93 +25,93 @@ router.get("/", async(request, response) => {
     }
 });
 
-//feature 1
-router.post("/menu", async(request, response) => {
-    const menu = new menuModel(request.body);
+// //feature 1
+// router.post("/menu", async(request, response) => {
+//     const menu = new menuModel(request.body);
 
-    try {
-        await menu.save();
-        response.status(200).send({ status: "201", message: "OK", data: menu });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+//     try {
+//         await menu.save();
+//         response.status(200).send({ status: "201", message: "OK", data: menu });
+//     } catch (error) {
+//         response.status(500).send(error);
+//     }
+// });
 
-//feature 2
-router.get("/menu", async(request, response) => {
-    const menu = await menuModel.find({});
+// //feature 2
+// router.get("/menu", async(request, response) => {
+//     const menu = await menuModel.find({});
 
-    try {
-        response.status(200).send({ status: "201", message: "OK", data: menu });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-//feature 3
-router.get("/menu/:id", async(request, response) => {
-    const menu = await menuModel.findById(request.params.id);
-    console.log(menu)
-    try {
-        response.status(200).send(menu);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+//     try {
+//         response.status(200).send({ status: "201", message: "OK", data: menu });
+//     } catch (error) {
+//         response.status(500).send(error);
+//     }
+// });
+// //feature 3
+// router.get("/menu/:id", async(request, response) => {
+//     const menu = await menuModel.findById(request.params.id);
+//     console.log(menu)
+//     try {
+//         response.status(200).send(menu);
+//     } catch (error) {
+//         response.status(500).send(error);
+//     }
+// });
 
 
-//PUT Update menu by id
-router.put("/menu/:id", async(request, response) => {
+// //PUT Update menu by id
+// router.put("/menu/:id", async(request, response) => {
 
-    try {
-        const menu = await menuModel.findByIdAndUpdate(request.params.id, request.body);
-        await menu.save();
-        response.status(200).send(menu);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+//     try {
+//         const menu = await menuModel.findByIdAndUpdate(request.params.id, request.body);
+//         await menu.save();
+//         response.status(200).send(menu);
+//     } catch (error) {
+//         response.status(500).send(error);
+//     }
+// });
 
-//DELETE menu using id
-router.delete("/menu/:id", async(request, response) => {
-    try {
-        const menu = await menuModel.findByIdAndDelete(request.params.id);
+// //DELETE menu using id
+// router.delete("/menu/:id", async(request, response) => {
+//     try {
+//         const menu = await menuModel.findByIdAndDelete(request.params.id);
 
-        if (!menu) response.status(404).send("No item found");
-        response.status(200).send("Item Deleted ");
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+//         if (!menu) response.status(404).send("No item found");
+//         response.status(200).send("Item Deleted ");
+//     } catch (error) {
+//         response.status(500).send(error);
+//     }
+// });
 
-//create POST meal
-router.post("/order", async(request, response) => {
-    const { menuItemId, userPhoneNumber } = request.body;
-    const order = new orderModel(request.body);
-    const menu = await menuModel.findById(menuItemId).populate("order");
+// 
+// router.post("/order", async(request, response) => {
+//     const { menuItemId, userPhoneNumber } = request.body;
+//     const order = new orderModel(request.body);
+//     const menu = await menuModel.findById(menuItemId).populate("order");
 
-    // Use the service
-    const options = {
-        to: userPhoneNumber,
-        message: `Your order will be ready soon.`,
-    };
+//     // Use the service
+//     const options = {
+//         to: userPhoneNumber,
+//         message: `Your order will be ready soon.`,
+//     };
 
-    try {
-        await order.save();
+//     try {
+//         await order.save();
 
-        //sendSMS.JS
-        sms
-            .send(options)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        response.status(200).send(request.body);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+//         //sendSMS.JS
+//         sms
+//             .send(options)
+//             .then((response) => {
+//                 console.log(response);
+//             })
+//             .catch((error) => {
+//                 console.log(error);
+//             });
+//         response.status(200).send(request.body);
+//     } catch (error) {
+//         response.status(500).send(error);
+//     }
+// });
 
 //list GET order
 router.get("/orders", async(request, response) => {
